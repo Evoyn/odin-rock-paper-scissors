5a243b2b81903f098486c150f51821bc6068c86b
ehanstdy <ehannstudy@gmail.com>
2025-02-17 07:19:18 +0700

    Update - Add function getComputerChoice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("test");
+function getComputerChoice() {
+  randomChoice = Math.floor(Math.random() * 3);
+
+  if (randomChoice === 0) {
+    return "Rock";
+  } else if (randomChoice === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
